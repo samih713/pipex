443678d5ae1287c60a756645fd191895ff921c30
samih713 <sakmohamed@gmail.com>
2023-02-13 03:56:18 +0400

    1 loop to rule them all

--- pipex/bonus/pipex_bonus.c
+++ pipex/bonus/pipex_bonus.c
@@ -3,90 +3,84 @@
 /*                                                        :::      ::::::::   */
 /*   pipex_bonus.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sabdelra <sabdelra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sabdelra <sabdelra@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 15:15:46 by sabdelra          #+#    #+#             */
-/*   Updated: 2023/02/12 22:46:16 by sabdelra         ###   ########.fr       */
+/*   Updated: 2023/02/13 03:54:28 by sabdelra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
+/* TO-FIX */
+/* MULTIPLE PIPE */
+/* SIMPLIFY */
+/* IMPROVE MAKE */
 
-static void	child_one(char *cmd, int fd[], int pipe_fd[], char **envp);
-static void	child_two(char *cmd, int fd[], int pipe_fd[], char **envp);
-static void	close_fds(int *pipe_fd, int *fd);
+static void	close_fds(int *p_fd, int *fd);
+
+enum {
+	READ_END = 0,
+	WRITE_END = 1,
+	INFILE = 2,
+	OUTFILE = 3
+};
 
 int	main(int argc, char **argv, char **envp)
 {
-	int	fd[2];
-	int	pipe_fd[2];
-	int	child[2];
-	int	pstat;
-	int i;
+	// VARIABLES
+	int fd[4]; // [0,1 -> pipe; 2,3 -> infile/outfile]
+	int	ch_pid;
+	// int	pstat;
+	// int i;
+	// VARIABLES END
 
-	i = 3;
-	arg_error(argc);
-	fd[0] = open_file(argv[1], INFILE);
-	fd[1] = open_file(argv[argc - 1], OUTFILE);
-	if (pipe(pipe_fd) < 0)
+	// arg_error(argc);
+	//
+	fd[INFILE] = open_file(argv[1], INFILE);
+	fd[OUTFILE] = open_file(argv[argc - 1], OUTFILE);
+	if (pipe(fd) < 0)
 		msg_error("pipe()");
-	child[0] = fork();
-	if (child[0] < 0)
-		msg_error("fork");
-	if (child[0] == 0)
-		child_one(argv[2], fd, pipe_fd, envp);
-	while (child[0] != 0 && i < argc - 2)
-	{
-		child[0] = fork();
-		if (child[0] < 0)
-			msg_error("fork");
-		if (child[0] == 0)
-		{
-			printf("hello\n");
-			dup2(pipe_fd[WRITE_END], STDOUT_FILENO);
-			dup2(pipe_fd[READ_END], STDIN_FILENO);
-			close(fd[0]);
-			close(fd[1]);
-			exec_cmd(argv[i], envp);
+	// FIRST PROCESS
+
+	for (int i = argc - 4; i > 0; --i) {
+		ch_pid = fork();
+		if (!ch_pid) {
+			dup2(fd[INFILE], STDIN_FILENO);
+			dup2(fd[WRITE_END], STDOUT_FILENO);
+			close(fd[WRITE_END]);
+			exec_cmd(argv[i + 1], envp);
 		}
-		i++;
+		if (i == 1) {
+			close(fd[INFILE]);
+			fd[INFILE] = fd[READ_END]; // children start reading from pipe after first iteration
+		}
 	}
-	child[1] = fork();
-	if (child[1] < 0)
-		msg_error("fork");
-	if (child[1] == 0)
-		child_two(argv[argc - 2], fd, pipe_fd, envp); //
-	close_fds(pipe_fd, fd);
-	wait(&pstat);
-	return (0);
+	// MIDDLE PROCESS
+	dup2(fd[READ_END], STDIN_FILENO);
+	dup2(fd[WRITE_END], STDOUT_FILENO);
+	close(fd[0]);
+	close(fd[1]);
+	// // LAST PROCESS PARENT?
+	// dup2(p_fd[READ_END], STDIN_FILENO);
+	// dup2(fd[1], STDOUT_FILENO);
+	// exec_cmd(argv[i], envp);
+	close_fds(p_fd, fd);
+	// wait(&pstat);
+	// return (0);
 }
 
-static void	close_fds(int *pipe_fd, int *fd)
+static void	close_fds(int *p_fd, int *fd)
 {
-	close(pipe_fd[0]);
+	close(p_fd[0]);
 	close(fd[0]);
-	close(pipe_fd[1]);
+	close(p_fd[1]);
 	close(fd[1]);
 }
-
-static void	child_one(char *cmd, int fd[], int pipe_fd[], char **envp)
+// fork how many times?
+/* static void child(int *fd, int *p_fd, char *cmd, char *envp[])
 {
-	dup2(fd[0], STDIN_FILENO);
-	dup2(pipe_fd[WRITE_END], STDOUT_FILENO);
-	close(pipe_fd[READ_END]);
-	close(pipe_fd[WRITE_END]);
-	close(fd[1]);
-	exec_cmd(cmd, envp);
-}
 
-static void	child_two(char *cmd, int fd[], int pipe_fd[], char **envp)
-{
-	printf("child2");
-	dup2(fd[1], STDOUT_FILENO);
-	dup2(pipe_fd[READ_END], STDIN_FILENO);
-	close(pipe_fd[READ_END]);
-	close(pipe_fd[WRITE_END]);
-	close(fd[0]);
-	exec_cmd(cmd, envp);
-}
-/* -->  */
+	// COMMON
+	close(p_fd[READ_END]);
+	close(p_fd[WRITE_END]);
+} */
